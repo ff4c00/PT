e4f794f97828b2c3a7d0d85692ff048c8c9f04ee
mike <mikeposner@163.com>
2017-12-06 17:20:25 +0800

    片段提交

--- pt_sh/pt_record_part.sh
+++ pt_sh/pt_record_part.sh
@@ -1,3 +1,5 @@
+echo -e "请输入片段关键字:";read -e key;
+echo "`cd part/;ls | grep "${key}";`";
 echo -n "请输入片段的主题:";read -e title;
 
 # 判断文件是否存在.不存在则新建
@@ -21,9 +23,10 @@ done
 # 写入文件
 for ((i=0;i<${#contents[@]};i++))
 {
- echo -e "<!-- ${pt_date} --> \n<h3>${outlines[$i]}</h3>" | tee -ai $pt_file_path;
+ echo -e "<h3>\n${outlines[$i]}<br>\n<span class=\"gray\">${pt_date}</span>\n</h3>" | tee -ai $pt_file_path;
  echo -e "<p>\n${contents[$i]}\n</p> \n" | tee -ai $pt_file_path;
 }
 # 写入文件END
 
 echo "关于${title}的片段记录完毕."
+
